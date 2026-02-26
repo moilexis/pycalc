7db91b732cc89e025201190f85a3342bc6b5117f
Alexis Morel <22501205@me.uvsq.fr>
2026-02-26 11:46:46 +0100

    version complexe de la multiplication

--- pycalc.py
+++ pycalc.py
@@ -4,4 +4,9 @@ def additionne (a,b):
 	return a+b
 	
 def multiplie (a,b):
-	return a*b
+	resultat = 0
+	for i in range (abs(b)):
+		resultat = resultat +a
+	if b<0 : 
+		return -resultat
+	return resultat
